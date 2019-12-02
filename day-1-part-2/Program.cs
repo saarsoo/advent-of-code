7d51c51782f26d4749ef6f06838273b040bbf6a3
Johan Saarsoo <johan.saarsoo@fortnox.se>
2019-12-02 14:19:09 +0100

    Day-1 Part-2

---
+++ day-1-part-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace day_1_part_2
+{
+    class Program
+    {
+        static async Task Main(string[] masses)
+        {
+            var fuelValues = CalculateFuelAsync(masses.Select(m => int.Parse(m)));
+            await Console.Out.WriteLineAsync($"Total fuel: {await fuelValues.SumAsync()}");
+        }
+
+        private static async IAsyncEnumerable<int> CalculateFuelAsync(IEnumerable<int> masses) {
+            foreach (var mass in masses) {
+                var fuel = CalculateFuelForMass(mass);
+
+                await Console.Out.WriteLineAsync($"Require {fuel} fuel for {mass} mass");
+
+                yield return fuel;
+            }
+        }
+
+        private static int CalculateFuelForMass(int mass) {
+            var fuel = (mass / 3) - 2;
+
+            int fuelForFuel = fuel;
+            while ((fuelForFuel = (fuelForFuel / 3) -2) > 0) {
+                fuel += fuelForFuel;
+            }
+
+            return Math.Max(fuel, 0);
+        }
+    }
+}
